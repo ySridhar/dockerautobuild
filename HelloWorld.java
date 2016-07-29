492062f0cab212f9f60aabd0c0ac77c347ee5dfc
ySridhar <ysridhar@hotmail.co.in>
2016-07-29 16:59:14 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,5 @@ public class HelloWorld {
    public static void main(String[] args) {
       // Prints "Hello, World" in the terminal window.
       System.out.println("Hello, World");
-      System.out.println("Qick Change");
    }
 }
